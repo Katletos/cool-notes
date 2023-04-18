c424dfb00b5f1116d93ba73cb6e8df6ea9a9301c
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-04-18 22:15:42 +0300

    add registration and middleware

--- Core/functions.php
+++ Core/functions.php
@@ -4,7 +4,7 @@ function urlIs(string $value): bool
     return $_SERVER['REQUEST_URI'] == $value;
 }
 
-function authorize(bool $condition, $status = Response::FORBIDDEN)
+function authorize(bool $condition, $status = Response::FORBIDDEN): void
 {
     if (! $condition)
     {
@@ -17,7 +17,8 @@ function base_path(string $path):string
     return BASE_PATH . $path;
 }
 
-function view(string $path): string
+function view(string $path, $attributes = []): string
 {
-    return base_path('views/' . $path);
+    extract($attributes);
+    require base_path('views/' . $path);
 }
---
+++ controllers/notes/edit.php
@@ -0,0 +1,6 @@
+<?php
+
+view('notes/create.view.php', [
+    'heading' => 'My Notes',
+    'errors' => [],
+]);
